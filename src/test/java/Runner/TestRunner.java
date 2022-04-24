9dbd72d63162e645a08567b2ea6c164cf8e3492e
Daniel Farias <dj.luis198@gmail.com>
2022-04-24 19:24:39 -0300

    cambio en runner

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -8,11 +8,11 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features ="C:\\Users\\Daniel\\eclipse-workspace\\CucumberAutomationPage\\src\\test\\java\\Features",
+		features ="./src/test/java/Features",
 		glue = "TestGlue",
 		monochrome = true,
 		plugin = {"json:target/cucumber.json"},
-		tags = "  @pay",
+		//tags = "  @pay",
 		snippets = SnippetType.CAMELCASE)
 
 public class TestRunner {
